73d6fcbe5c9de76fb32e8ae6e0050810f7e3f1ea
Minghao Xu <i.violinsolo@gmail.com>
2018-05-13 00:27:09 +0800

    add password test page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {minimum: 5, maximum: 20},
             format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+  has_secure_password
 end
