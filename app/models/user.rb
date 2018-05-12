9c98c2022e9987561e70d15692e608b1653a5249
Minghao Xu <i.violinsolo@gmail.com>
2018-05-13 00:08:53 +0800

    add validates in name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true, length: {minimum: 5, maximum: 20},
+            format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
