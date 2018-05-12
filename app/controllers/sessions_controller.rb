62feb546d69143f8dcf5d21d8565d2b148bde297
Minghao Xu <i.violinsolo@gmail.com>
2018-05-13 07:40:57 +0800

    add flash back

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # 登入用户，然后重定向到用户的资料页面
+    else
+      # 创建一个错误消息
+      flash[:danger] = 'Invalid email/password combination' # 不完全正确
+      render 'new'
+    end
   end
 
   def destroy
