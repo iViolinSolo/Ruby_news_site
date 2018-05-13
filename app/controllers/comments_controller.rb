57639210a692b5ba08eb6781013e39d9173e86e9
ViolinSolo <i.violinsolo@gmail.com>
2018-05-14 03:25:39 +0800

    add validate

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,19 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @school_news = Schoolnews.find(params[:schoolnews_id])
 
+    if @school_news.comments.create(comment_params)
+      flash[:success] = "Comment Success!"
+      redirect_to newcomments_path
+    else
+      render 'schoolnews/new'
+    end
   end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:content, :author)
+    end
 end
