b5add9bbff286a4d9f39d38a1dcaa7fbafa81d77
ViolinSolo <i.violinsolo@gmail.com>
2018-05-14 04:00:28 +0800

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,8 @@ Rails.application.routes.draw do
   get    '/newcomments',to: 'comments#newcomments'
 
   resources :users
-  resources :schoolnews do
-    resources :comments
-  end
+  resources :schoolnews
+  resources :comments
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
