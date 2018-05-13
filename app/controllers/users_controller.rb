dcf15a134e2e870e17c8253c5c26f9cde0267b13
Minghao Xu <i.violinsolo@gmail.com>
2018-05-13 13:59:08 +0800

    add pre check

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show, :edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -26,4 +27,14 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
+    # 前置过滤器
+    # 确保用户已登录
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
 end
