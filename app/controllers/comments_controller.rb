731524c7bf2252064d3cfb42b4c6d50a305d5315
ViolinSolo <i.violinsolo@gmail.com>
2018-05-14 04:16:04 +0800

    add permission

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :logged_in_user, only: [:new, :show, :create]
+
   COMM_NUM = 3
 
   def newcomments
@@ -23,6 +25,16 @@ class CommentsController < ApplicationController
 
     def comment_params
       params.require(:comment).permit(:content, :author, :schoolnews_id)
+    end
+
+    # 前置过滤器
+    # 确保用户已登录
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
 
 end
--- app/controllers/schoolnews_controller.rb
+++ app/controllers/schoolnews_controller.rb
@@ -1,5 +1,5 @@
 class SchoolnewsController < ApplicationController
-  before_action :logged_in_user, only: [:new, :edit, :update]
+  before_action :logged_in_user, only: [:new, :show, :create]
 
   PAGE_NUM = 8
 
