b8898a478227173f9ea6684619a7b709498b183f
Minghao Xu <i.violinsolo@gmail.com>
2018-05-13 18:12:31 +0800

    get page number

--- app/controllers/schoolnews_controller.rb
+++ app/controllers/schoolnews_controller.rb
@@ -4,6 +4,9 @@ class SchoolnewsController < ApplicationController
   def index
     @page_num = params[:p]
     @page_num = 0 if @page_num.nil?
+    @page_num = @page_num.to_i
+
+    @next_page_num = @page_num + 1
     # puts @page_num
     # render plain: @page_num
     @news_list = Schoolnews.order(created_at: :desc).offset(@page_num * PAGE_NUM).limit(PAGE_NUM)
