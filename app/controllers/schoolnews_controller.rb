ae10788bee46cd975ddf9b2b4b4e2ca6655ae0a9
Minghao Xu <i.violinsolo@gmail.com>
2018-05-13 18:14:02 +0800

    add pagenite

--- app/controllers/schoolnews_controller.rb
+++ app/controllers/schoolnews_controller.rb
@@ -6,7 +6,6 @@ class SchoolnewsController < ApplicationController
     @page_num = 0 if @page_num.nil?
     @page_num = @page_num.to_i
 
-    @next_page_num = @page_num + 1
     # puts @page_num
     # render plain: @page_num
     @news_list = Schoolnews.order(created_at: :desc).offset(@page_num * PAGE_NUM).limit(PAGE_NUM)
