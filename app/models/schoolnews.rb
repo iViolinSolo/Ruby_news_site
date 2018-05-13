e4413fc40864aaecb20ea47acd1cc7bd19bec8bd
ViolinSolo <i.violinsolo@gmail.com>
2018-05-13 19:54:23 +0800

    add validates

--- app/models/schoolnews.rb
+++ app/models/schoolnews.rb
@@ -1,4 +1,7 @@
 class Schoolnews < ApplicationRecord
   has_many :comments, dependent: :destroy
-  # validates
+  validates :title, presence: true, allow_blank: true, length: {minimum: 10, maximum: 200}
+
+  VALID_URL_REGEX = /(https?|ftp|file):\/\/[-A-Za-z0-9+&@#\/%?=~_|!:,.;]+[-A-Za-z0-9+&@#\/%=~_|]/i
+  validates :source, format: {with: VALID_URL_REGEX}, allow_nil: true  # Allow nil but if not empty, match regex
 end
