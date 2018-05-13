63d620d3dee2ff8e2e8b485f5150521808c04f5e
ViolinSolo <i.violinsolo@gmail.com>
2018-05-13 21:08:46 +0800

    add new comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,6 @@ class CommentsController < ApplicationController
   COMM_NUM = 3
 
   def newcomments
-    @news_list = Comment.order(created_at: :desc).limit(COMM_NUM)
+    @newest_comments = Comment.order(created_at: :desc).limit(COMM_NUM)
   end
 end
