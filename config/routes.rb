067d8a4a3651269564a2a37a11ecb906074503a7
Minghao Xu <i.violinsolo@gmail.com>
2018-05-13 08:16:00 +0800

    add root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
+  root   'static_pages#home'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   get    '/signup',  to: 'users#new'
   get    'users/show'
+  get    'static_pages/home'
+  get    'static_pages/help'
 
   resources :users
-
-  get   'static_pages/home'
-
-  get   'static_pages/help'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
