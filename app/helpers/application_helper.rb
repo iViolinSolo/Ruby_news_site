8c4f7912937211e1131bc8798f29df5fb7a28d26
ViolinSolo <i.violinsolo@gmail.com>
2018-05-13 22:21:15 +0800

    add time generator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,29 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
+
+  def cal_time_before(dt)
+    puts dt
+    origin = dt.to_i
+    # puts origin
+    now = Time.now.to_i
+
+    difference = now - origin
+    one_minute = 60
+    one_hour = 60 * 60
+    one_day = 60 * 60 * 24
+    one_year = 60 * 60 * 24 * 365
+
+    if difference < one_minute
+      pluralize(difference, 'second')
+    elsif one_minute <= difference && difference < one_hour
+      pluralize(difference / one_minute, 'minute')
+    elsif one_hour <= difference && difference < one_day
+      pluralize(difference / one_hour, 'hour')
+    elsif one_day <= difference && difference < one_year
+      pluralize(difference / one_day, 'day')
+    elsif one_year <= difference
+      pluralize(difference / one_year, 'year')
+    end
+  end
 end
