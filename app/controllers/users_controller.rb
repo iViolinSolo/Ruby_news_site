e478a1cd95536600e52aa28bbb6bc303ca46bf9e
Minghao Xu <i.violinsolo@gmail.com>
2018-05-13 14:31:41 +0800

    refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
     # 确保是正确的用户
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless @user == current_user
+      redirect_to(root_url) unless current_user?(@user)
     end
 
 end
