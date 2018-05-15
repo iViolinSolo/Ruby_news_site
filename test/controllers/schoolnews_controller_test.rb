7c5a6be48057719880cb766a1e1fe2e371c9f0ca
ViolinSolo <i.violinsolo@gmail.com>
2018-05-15 20:35:18 +0800

    add right case

--- test/controllers/schoolnews_controller_test.rb
+++ test/controllers/schoolnews_controller_test.rb
@@ -3,7 +3,9 @@ require 'test_helper'
 class SchoolnewsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get schoolnews_new_url
-    assert_response :success
+    # assert_response :success
+    # you need to login before go to this url
+    assert_redirected_to login_url
   end
 
 end
