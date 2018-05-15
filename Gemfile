bb8da756f0767159b8efb4d8309bbfcf4dce75ca
ViolinSolo <i.violinsolo@gmail.com>
2018-05-15 20:12:10 +0800

    test init

--- Gemfile
+++ Gemfile
@@ -55,6 +55,13 @@ group :development do
   gem 'web-console', '>= 3.3.0'
 end
 
+group :test do
+  gem 'rails-controller-testing', '1.0.2'
+  gem 'minitest-reporters',       '1.1.14'
+  gem 'guard',                    '2.13.0'
+  gem 'guard-minitest',           '2.4.4'
+end
+
 group :production do
   # for heroku using
   gem 'pg', '0.20.0'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+#--- temprary not use the following parts
+# require "minitest/reporters"
+# Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
