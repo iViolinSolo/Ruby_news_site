a6d26864fb251a93d01a0f39f9493c068a26fe97
ViolinSolo <i.violinsolo@gmail.com>
2018-05-19 12:06:13 +0800

    add test in user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,12 +9,17 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(name: "ViolinSolo4894", email: "33f@le.com", password: "foobar1@FOOBAR", password_confirmation: "foobar1@FOOBAR")
   end
 
-  test "should not be valid" do
+  test "should be valid" do
     assert @user.valid?
   end
 
   test "name should be present" do
     @user.name = "     "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
     assert_not @user.valid?
   end
 
@@ -29,10 +34,15 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  test "name should not be too long" do
-    @user.name = "a" * 51
+  test "email should match the regex" do
+    @user.email = "sd.com"
+    assert_not @user.valid?
+    @user.email = "sd@com"
+    assert_not @user.valid?
+    @user.email = "@ss.com"
     assert_not @user.valid?
   end
+
 
   test "email should not be too long" do
     @user.email = "a" * 244 + "@example.com"
@@ -53,6 +63,17 @@ class UserTest < ActiveSupport::TestCase
 
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+  test "password should match the regex" do
+    @user.password = @user.password_confirmation = "Ab@foobarfoobar"
+    assert_not @user.valid?
+    @user.password = @user.password_confirmation = "Ab1foobarfoobar"
+    assert_not @user.valid?
+    @user.password = @user.password_confirmation = "ab1@foobarfoobar"
+    assert_not @user.valid?
+    @user.password = @user.password_confirmation = "AB1@FOOBARFOOBAR"
     assert_not @user.valid?
   end
 end
