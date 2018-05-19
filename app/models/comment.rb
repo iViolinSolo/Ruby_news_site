e55f28f15543c5f7a602afae16db3c844620289c
ViolinSolo <i.violinsolo@gmail.com>
2018-05-19 11:32:07 +0800

    add the right regex

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :schoolnews
 
-  VALID_CONTENT_REGEX = /\A\s*\S\s*\S\s*\S\s*\z/x
-  validates :content, presence: true, length: {maximum: 1000, minimum: 3}#,
-            #format: { with: VALID_CONTENT_REGEX }
+  VALID_CONTENT_REGEX = /\A\s*\S\s*\S\s*\S\s*/x
+  validates :content, presence: true, length: {maximum: 1000, minimum: 3},
+            format: { with: VALID_CONTENT_REGEX }
   validates :author, presence: true
 end
