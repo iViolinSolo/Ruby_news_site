65e586b75ef98c20492b31719a4c0994e3e59d8d
ViolinSolo <i.violinsolo@gmail.com>
2018-05-20 15:09:21 +0800

    add more test features

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,5 +15,22 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'sessions/new'
 
+    # assert_select 'div#<CSS id for error explanation>'
+    # assert_select 'div.<CSS class for field with error>'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "ViolinSolo6374",
+                                         email: "user@example.com",
+                                         password:              "foobar1@FOOBAR",
+                                         password_confirmation: "foobar1@FOOBAR" } }
+    end
+    follow_redirect!
+    assert_template 'schoolnews/index'
+    assert flash[:success] == "Welcome to the School News App!"
   end
 end
