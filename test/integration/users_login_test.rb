832f229b9f30e2de2b39f4f13eaffcc9a3b3bc9a
ViolinSolo <i.violinsolo@gmail.com>
2018-05-20 16:27:39 +0800

    add more test features add fixture

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+  end
 
   test "login with invalid information" do
     get login_path
@@ -15,5 +15,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'foobar1@FOOBAR' } }
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_template 'schoolnews/index'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
